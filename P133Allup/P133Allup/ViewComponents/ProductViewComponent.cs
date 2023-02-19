0cc5a79e9ba74be86a1a016dae878ddf7d57cedc
Sonava <sonava@code.edu.az>
2023-02-20 02:24:57 +0400

    Fourth Commit

---
+++ P133Allup/P133Allup/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P133Allup.DataAccessLayer;
+
+namespace P133Allup.ViewComponents
+{
+    public class ProductViewComponent :ViewComponent
+       
+    {
+        private readonly AppDbContext _context;
+        public ProductViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
